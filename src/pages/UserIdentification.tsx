f7b60c8fa3125831e8f1e48ed7a0e2d9132db8e9
Rafael Hirooka <rafaelhirooka16@gmail.com>
2021-04-22 22:53:37 -0300

    navigation and fonts

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,134 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useState, useCallback } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  View, 
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard
+} from 'react-native';
+
+import { Button } from '../components/button';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>();
+
+  const navigation = useNavigation();
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputChange = useCallback((value: string) => {
+    setIsFilled(!!value);
+    setName(value);
+  }, []);
+
+  const handleSubmit = useCallback(() => {
+    navigation.navigate('Confirmation');
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView 
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <View style={styles.heading}>
+                <Text style={styles.emoji}>
+                  {isFilled ? '😁' : '😀' }
+                </Text>
+                <Text style={styles.title}>
+                  Como podemos {'\n'} 
+                  chamar você?
+                </Text>
+              </View>
+
+              <TextInput
+                style={[
+                  styles.input,
+                  (isFocused || isFilled) && { borderColor: colors.green }
+                ]}
+                placeholder="Digite um nome"
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
+              
+              <View style={styles.footer}>
+                <Button 
+                  title="Confirmar" 
+                  onPress={handleSubmit}
+                />
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  content: {
+    flex: 1,
+    width: '100%'
+  },
+  heading: {
+    alignItems: 'center'
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 54,
+  },
+  emoji: {
+    fontSize: 44
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: colors.gray,
+    color: colors.heading,
+    width: '100%',
+    fontSize: 18,
+    marginTop: 50,
+    padding: 10,
+    textAlign: 'center'
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 32,
+    marginTop: 20
+  },
+  footer: {
+    width: '100%',
+    marginTop: 40,
+    paddingHorizontal: 20
+  }
+})
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,53 +1,86 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useCallback } from 'react';
+import { 
+  Text, 
+  Image, 
+  TouchableOpacity, 
+  StyleSheet, 
+  SafeAreaView,
+  Dimensions,
+  View
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  const handleStart = useCallback(() => {
+    navigation.navigate('UserIdentification');
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
-      </Text>
-
-      <Image source={wateringImg} style={styles.image} />
-
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas.
-        Nós cuidamos de lembrar sempre que precisar.
-      </Text>
-
-      <TouchableOpacity 
-        style={styles.button} 
-        activeOpacity={0.7}
-      >
-        <Text style={styles.buttonText}>
-          >
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma fácil
         </Text>
-      </TouchableOpacity>
+
+        <Image
+          source={wateringImg}
+          style={styles.image}
+          resizeMode="contain"
+        />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidamos de lembrar sempre que precisar.
+        </Text>
+
+        <TouchableOpacity 
+          style={styles.button} 
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather
+            name="chevron-right"
+            style={styles.buttonIcon}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
   button: {
     backgroundColor: colors.green,
@@ -58,12 +91,11 @@ const styles = StyleSheet.create({
     height: 56,
     width: 56
   },
-  image: {
-    width: 292,
-    height: 284
-  },
-  buttonText: {
+  buttonIcon: {
     color: colors.white,
-    fontSize: 24
-  }
+    fontSize: 28
+  },
+  image: {
+    height: Dimensions.get('window').width * 0.7
+  },
 });
